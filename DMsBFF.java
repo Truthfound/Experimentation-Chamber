8b7a26dfd7a5856d652aece1581d89b29ba5a3d0
Truthless <bluefire.luke@gmail.com>
2013-11-11 21:51:25 -0700

    D&D Try 2

--- DMsBFF.java
+++ DMsBFF.java
@@ -1,6 +1,38 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
 public class DMsBFF {
 	public static void main(String[] args) {
-		
+		DMsBFF d = new DMsBFF();
+		d.runMyFrame();
+		Player john = new Player(12, 14, 10, 16, 18, 30);
+		System.out.println("Str = " + john.Mod(john.Str));
+		System.out.println("Dex = " + john.Mod(john.Dex));
+		System.out.println("Con = " + john.Mod(john.Con));
+		System.out.println("Int = " + john.Mod(john.Int));
+		System.out.println("Wis = " + john.Mod(john.Wis));
+		System.out.println("Cha = " + john.Mod(john.Cha));
+	}
+	public void runMyFrame(){
+		JFrame stats = new JFrame();
+		JButton b1 = new JButton("Stats");
+		b1.setVerticalTextPosition(AbstractButton.CENTER);
+		b1.setHorizontalTextPosition(AbstractButton.LEADING);
+		b1.setActionCommand("Press");
+		b1.addActionListener((ActionListener) this);
+		JButton b2 = new JButton("Items");
+		b2.setVerticalTextPosition(AbstractButton.CENTER);
+		b2.setHorizontalAlignment(AbstractButton.LEADING);
+		b2.setActionCommand("Press");
+		b2.addActionListener((ActionListener) this);
+	}
+	public void actionPerformed(ActionEvent e){
+		if ("Press".equals(e.getActionCommand())){
+			System.out.println("Test Successful!");
+		}
+		else {
+			System.exit(0);
+		}
 	}
 
 }
--- Player.java
+++ Player.java
@@ -1,4 +1,21 @@
-
+import javax.swing.JFrame;
+import javax.swing.*;
 public class Player {
-
+	public static int Str;
+	public static int Dex;
+	public static int Con;
+	public static int Int;
+	public static int Wis;
+	public static int Cha;
+	public Player(int Str1, int Dex1, int Con1, int Int1, int Wis1, int Cha1){
+		Str = Str1;
+		Dex = Dex1;
+		Con = Con1;
+		Int = Int1;
+		Wis = Wis1;
+		Cha = Cha1;
+	}
+	public int Mod(int stat){
+		return (stat/2) - 5;
+	}
 }
